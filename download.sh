2c19be2b2548e5cd8512cf3568cf48b0c6b54207
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2024-08-26 13:38:34 +0800

    Add download script

---
+++ download.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Array of plugin URLs
+plugins=(
+    "https://downloads.wordpress.org/plugin/woocommerce-payments.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-gateway-stripe.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-gateway-mercadopago.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-paypal-payments.zip"
+    "https://downloads.wordpress.org/plugin/mollie-payments-for-woocommerce.zip"
+    "https://downloads.wordpress.org/plugin/woo-razorpay.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-payfast-gateway.zip"
+    "https://downloads.wordpress.org/plugin/payu-india.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-square.zip"
+    "https://downloads.wordpress.org/plugin/klarna-payments-for-woocommerce.zip"
+    "https://downloads.wordpress.org/plugin/klarna-checkout-for-woocommerce.zip"
+    "https://downloads.wordpress.org/plugin/woo-paystack.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-gateway-eway.zip"
+    "https://downloads.wordpress.org/plugin/woocommerce-gateway-amazon-payments-advanced.zip"
+    "https://downloads.wordpress.org/plugin/afterpay-gateway-for-woocommerce.zip"
+    "https://downloads.wordpress.org/plugin/zipmoney-payments-woocommerce.zip"
+    "https://downloads.wordpress.org/plugin/airwallex-online-payments-gateway.zip"
+    "https://downloads.wordpress.org/plugin/payoneer-checkout.zip"
+)
+
+# Download and unzip each plugin
+for plugin_url in "${plugins[@]}"; do
+    # Extract the file name from the URL
+    filename=$(basename "$plugin_url")
+    
+    # Download the plugin
+    curl -O "$plugin_url"
+    
+    # Unzip the plugin
+    unzip -o "$filename" -d "./plugins"
+    
+    # Remove the ZIP file after extraction
+    rm "$filename"
+done
+
+echo "All plugins downloaded and unzipped successfully."
+
